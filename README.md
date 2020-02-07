4af16fc155c56f53ce0f43be2b0784ff4456d283
VincentMarrec <marrecvincent29@gmail.com>
2020-02-07 18:12:20 +0100

    File upload template with apollon

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# apollon-demos
+# apollon-demos : File upload with apollon
+
+## Getting started
+
+1. Install the dependencies
+
+        npm install
+
+2. Run apollon server with npm
+
+        npm run start
+
+3. Upload a file with curl
+
+        curl localhost:3000 \
+        -F operations='{ "query": "mutation ($file: Upload!) { file_upload(file: $file) }" }' \
+        -F map='{ "0": ["variables.file"] }' \
+        -F 0=@yarn.lock
--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,25 @@
 export default async function(){
     
-    this.Query.hello = _ => "Hello world"
+    this.Mutation.file_upload = async function file_upload(root, { file }, { logger }){
+        const { createReadStream, filename, mimetype, encoding } = await file
+            .catch(err => {
+                logger.error("An error occurred while trying to read the file", err);
+                throw "An error occurred while trying to read the file";
+            });
+
+        await new Promise((resolve, reject) => {
+            createReadStream()
+                .on('data', data => {
+                    // Process data here
+                    logger.info("Data received", data);
+                })
+                .on('error', error => {
+                    logger.error("An error occurred while processing the data", error);
+                    throw "An error occurred while processing the data";
+                })
+                .on('end', resolve);
+        });
+
+        return true;
+    };
 }
